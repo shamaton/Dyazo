136e35eda8520e7d631ba026e5d950a3f72b0944
Masayuki Shamoto <syamoto.masayuki@donuts.ne.jp>
2015-12-18 15:31:38 +0900

    run.shを追加

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export PRODUCTION=1
+
+start() {
+    cd /home/gyazo/git/Dyazo;
+    exec nohup /home/gyazo/git/Dyazo/main > /tmp/gyazo.out 2>&1&
+    echo $! > /home/gyazo/pids/dyazo.pid
+    disown
+}
+
+stop() {
+    kill `cat /home/gyazo/pids/dyazo.pid`
+}
+
+
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+  echo "usage: run.sh {start|stop|restart}" ;;
+esac
+exit 0
